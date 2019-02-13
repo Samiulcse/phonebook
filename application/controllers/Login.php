bae63d1fb479f4ef1ce1a69d7f4c7e7635d7cb97
Samiul Islam <samiulcse@yahoo.com>
2019-02-13 17:43:17 +0600

    Phonebook project created

---
+++ application/controllers/Login.php
@@ -0,0 +1,85 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Login extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->model('Login_model', 'auth_model', true);
+    }
+
+    public function index()
+    {
+
+        $this->load->view('auth/auth');
+
+    }
+
+    public function login_validation()
+    {
+
+        $this->load->view('auth/auth');
+
+    }
+
+    // regiter user
+
+    public function register_user()
+    {
+
+        // $this->load->library('form_validation');
+
+        // $this->form_validation->set_rules('user_name', 'User Name', 'required|min_length[5]|max_length[12]|is_unique[users.user_name]',
+        //     array(
+        //         'required' => 'You have not provided %s.',
+        //         'is_unique' => 'This %s already exists.',
+        //     ));
+
+        // $this->form_validation->set_rules('user_email', 'Email', 'trim|required|valid_email|is_unique[users.user_email]');
+
+        // $this->form_validation->set_rules('user_pass', 'Password', 'required|min_length[5]');
+
+        // $this->form_validation->set_rules('user_confirm_pass', 'Password', 'required|matches[user_pass]');
+
+        $config = array(
+            array(
+                'field' => 'user_name',
+                'label' => 'Username',
+                'rules' => 'required|min_length[5]|max_length[12]|is_unique[users.user_name]',
+                'errors' => array(
+                    'required' => 'You have not provided %s.',
+                    'is_unique' => 'This %s already exists.',
+                ),
+            ),
+            array(
+                'field' => 'user_email',
+                'label' => 'Username',
+                'rules' => 'trim|required|valid_email|is_unique[users.user_email]',
+                'errors' => array(
+                    'required' => 'You have not provided %s.',
+                    'is_unique' => 'This %s already exists.',
+                ),
+            ),
+        );
+
+        $this->form_validation->set_rules($config);
+
+        if ($this->form_validation->run()) {
+
+            print_r($this->input->post());
+
+        } else {
+
+            echo form_error('user_name');
+
+            exit;
+
+            redirect(base_url('#toregister'));
+
+        }
+    }
+
+}
---
+++ phonebook.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 13, 2019 at 12:26 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `phonebook`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(30) NOT NULL,
+  `user_email` varchar(50) NOT NULL,
+  `user_pass` varchar(40) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `user_name`, `user_email`, `user_pass`, `created_at`) VALUES
+(1, 'samiul', 'samiul@gmail.com', '21232f297a57a5a743894a0e4a801fc3', '2019-02-13 00:00:00');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
